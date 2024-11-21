effcb2f8fb26658565abd70b78ee88ccfdd00b44
Nicky Nocerino <bleached128@gmail.com>
2024-11-20 21:19:12 -0500

    simple file support

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,26 @@ use egui_file_dialog::FileDialog;
 use otp_exchange::otp::OneTimePad;
 
 struct MyApp {
-    file_dialog: FileDialog,
-    selected_file: Option<PathBuf>,
-    pad: Option<OneTimePad>,
-    pad_loc: String,
+    pad_select_dialog: FileDialog,
+    target_select_dialog: FileDialog,
+    pad_zip_path: Option<PathBuf>,
+    pad_data: Option<OneTimePad>,
+    pad_bin: String,
+    target_path: Option<PathBuf>,
+    decrypt_target: String,
 }
 
 impl MyApp {
     pub fn new(_cc: &eframe::CreationContext) -> Self {
         Self {
             // Create a new file dialog object
-            file_dialog: FileDialog::new(),
-            selected_file: None,
-            pad: None,
-            pad_loc: "./bin/pad_temp".to_string()
+            pad_select_dialog: FileDialog::new(),
+            target_select_dialog: FileDialog::new(),
+            pad_zip_path: None,
+            pad_data: None,
+            pad_bin: "./bin/pad_temp".to_string(),
+            target_path: None,
+            decrypt_target: "".to_string(),
         }
     }
 }
@@ -29,22 +35,45 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             if ui.button("Select Pad Zip File").clicked() {
                 // Open the file dialog to select a file.
-                self.file_dialog.select_file();
+                self.pad_select_dialog.select_file();
             }
-
-            ui.label(format!("Pad Zip: {:?}", self.selected_file));
 
             // Update the dialog
-            self.file_dialog.update(ctx);
+            self.pad_select_dialog.update(ctx);
 
             // Check if the user selected a file.
-            if let Some(path) = self.file_dialog.take_selected() {
-                self.selected_file = Some(path.to_path_buf());
+            if let Some(path) = self.pad_select_dialog.take_selected() {
+                self.pad_zip_path = Some(path.to_path_buf());
             }
 
-            if self.selected_file.clone().is_some() {
-                self.pad = Some(OneTimePad::load_zip(&self.selected_file.clone().unwrap().display().to_string(), &self.pad_loc));
-                ui.label(format!("{:?} Bytes of Pad Remain", self.pad.as_ref().unwrap().remaining()));
+            if self.pad_zip_path.clone().is_some() {
+
+                ui.label(format!("Pad Zip: {:?}", self.pad_zip_path.clone().unwrap()));
+                if !self.pad_data.is_some() {
+                    self.pad_data = Some(OneTimePad::load_zip(&self.pad_zip_path.clone().unwrap().display().to_string(), &self.pad_bin));
+                }
+                ui.label(format!("{:?} Bytes of Pad Remain", self.pad_data.as_ref().unwrap().remaining()));
+                if ui.button("Select Target File").clicked() {
+                    // Open the file dialog to select a file.
+                    self.target_select_dialog.select_file();
+                }
+
+                self.target_select_dialog.update(ctx);
+
+                // Check if the user selected a file.
+                if let Some(path) = self.target_select_dialog.take_selected() {
+                    self.target_path = Some(path.to_path_buf());
+                }
+
+                if self.target_path.clone().is_some() {
+                    ui.label(format!("target path: {:?}", self.target_path.clone().unwrap()));
+                    ui.label("What Would You Like To Name The Decrypted File?");
+                    let response = ui.add(egui::TextEdit::singleline(&mut self.decrypt_target));
+                    if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                        self.pad_data.as_mut().expect("decrypting without a pad").decrypt_file(&self.target_path.clone().unwrap().display().to_string(), &self.decrypt_target);
+                        self.target_path = None;
+                    }
+                }
             }
         });
     }
